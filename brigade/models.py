7509bb1d13be2ba4ddea1ed318944340f30bdeda
MurodzodaMs <murodzodams07@gmail.com>
2026-01-25 17:28:01 +0500

    add brigade

--- brigade/models.py
+++ brigade/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Brigade(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.ForeignKey("services.Category", 
+        related_name="brigades", on_delete=models.CASCADE)
+    rating = models.DecimalField(max_digits=3, decimal_places=2, default=0)
+    description = models.TextField(blank=True)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    phone = models.CharField(max_length=20)
+    city = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    
