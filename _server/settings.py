5b6b18a720eca4d2f5305650de302f8cdbf1ed8e
MurodzodaMs <murodzodams07@gmail.com>
2026-01-25 17:36:01 +0500

    change settings

--- _server/settings.py
+++ _server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#-3^pen_803y5@ocxip_)+hej3i*v4_#&g0fe&#(e)sjxg5jt9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', '34.55.198.124']
 
 
 # Application definition
